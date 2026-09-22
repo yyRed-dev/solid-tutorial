13a0f87faa85573580f3f0daee6d7d957a0b7004
Lucasfsilvalucas <fidelis.lsilva@gmail.com>
2026-09-22 10:10:49 -0300

    refatoracao de passageiro e asteroide)

--- src/solidexercicio10/model/Asteroide.java
+++ src/solidexercicio10/model/Asteroide.java
@@ -1,18 +1,8 @@
 package solidexercicio10.model;
 
-public class Asteroide {
-     private int x;
-    private int y;
+public class Asteroide extends EntidadeMapa {
 
     public Asteroide(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() { return x; }
-    public int getY() { return y; }
-
-    public boolean colideCom(Nave n) {
-        return n.getX() == x && n.getY() == y;
+        super(x, y);
     }
 }
--- src/solidexercicio10/model/Passageiro.java
+++ src/solidexercicio10/model/Passageiro.java
@@ -1,24 +1,23 @@
 package solidexercicio10.model;
 
 public class Passageiro {
-  private String nome;
-    private String tipo;
-    private int x;
-    private int y;
+  package solidexercicio10.model;
 
-    public Passageiro(String nome, String tipo, int x, int y) {
+public abstract class Passageiro extends EntidadeMapa {
+
+    private final String nome;
+
+    protected Passageiro(String nome, int x, int y) {
+        super(x, y);
         this.nome = nome;
-        this.tipo = tipo;
-        this.x = x;
-        this.y = y;
     }
 
-    public String getNome() { return nome; }
-    public String getTipo() { return tipo; }
-    public int getX() { return x; }
-    public int getY() { return y; }
-
-    public int getPontuacao() {
-        return 10;  // Default
+    public String getNome() {
+        return nome;
     }
+
+    public abstract int getPontuacao();
+
+    public abstract String getTipo();
+}
 }
