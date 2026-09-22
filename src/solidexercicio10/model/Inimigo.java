7102a5b56d5070117668ac6dd777aa433801586c
Lucasfsilvalucas <fidelis.lsilva@gmail.com>
2026-09-22 10:48:05 -0300

    refatoraçao de inimigo

--- src/solidexercicio10/model/Inimigo.java
+++ src/solidexercicio10/model/Inimigo.java
@@ -1,43 +1,20 @@
 package solidexercicio10.model;
 
-import java.util.Random;
-
-public class Inimigo {
-     private int x;
+public class Inimigo extends EntidadeMapa implements Movel{
+    private int x;
     private int y;
 
-    public Inimigo(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
+       public Inimigo(int x, int y) {
+        super(x, y);
     }
 
     public boolean colideCom(Nave n) {
         return n.getX() == x && n.getY() == y;
     }
 
-    public void mover(Random random, int minX, int maxX, int minY, int maxY) {
-        int direcao = random.nextInt(4);
-        switch (direcao) {
-            case 0:
-                if (x < maxX) x++;
-                break;
-            case 1:
-                if (x > minX) x--;
-                break;
-            case 2:
-                if (y < maxY) y++;
-                break;
-            case 3:
-                if (y > minY) y--;
-                break;
-        }
-    }
+  @Override
+public void mover(int dx, int dy) {
+    x += dx;
+    y += dy;
+}
 }
